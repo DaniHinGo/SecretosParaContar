dddccf2b890150c644e58e7e9724c2dd9b919202
Daniela <dhincapig1@eafit.edu.co>
2025-03-25 23:31:17 -0500

    Resolviendo compflictos

--- frontend/Katrina/app/root.tsx
+++ frontend/Katrina/app/root.tsx
@@ -8,7 +8,7 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
-import Menu from "./components/Menu";
+import Menu from "./components/menu";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
