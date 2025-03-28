f1fef266affdf27cdf051628dd454813ea93d829
Fabio Torres <fltorres1607@gmail.com>
2025-03-28 00:29:24 -0500

    cambios santiago a Dev

--- backend/VirtualBiblio.Business/obj/Debug/net9.0/VirtualBiblio.Business.AssemblyInfo.cs
+++ backend/VirtualBiblio.Business/obj/Debug/net9.0/VirtualBiblio.Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirtualBiblio.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d49496159abd31d29d96d5a9ded3c62fde18a42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9218485cfd0c38f84fd9bd5cb2333cba7309f82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirtualBiblio.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirtualBiblio.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
